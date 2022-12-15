acbf1aeb34850fdb332417fcec5c381829e76780
CGRocky122 <75678464+CGRocky122@users.noreply.github.com>
2022-12-15 21:22:28 +0100

    :sparkles: Création d'une promotion de A à Z

--- Biodevops - Azure.ps1
+++ Biodevops - Azure.ps1
@@ -45,7 +45,77 @@ function AADDisconnect{
     Disconnect-AzureAD
 }
 
+function createPromo{
+    [int]$yearPromotion = Read-Host "Enter the year of the promotion"
+    [int]$SelectacronymPromotion = Read-Host "[1] - PSSI - Pentesting & Security of Information Systems`n[2] - GPP - Public and Private Cloud Manager`n[3] - CPS - Project Management and Strategy`n[4] - ASI - Information Systems Architecture`nEnter a field of study"
+    Switch($SelectacronymPromotion){
+        1{$acronymPromotion = "PSSI"}
+        2{$acronymPromotion = "GPP"}
+        3{$acronymPromotion = "CPS"}
+        4{$acronymPromotion = "ASI"}
+    }
+    $namePromotion = "$yearPromotion"+"_"+"$acronymPromotion"
+    
+    try{
+        Write-Host "[*] Creation of an administrative unit" -ForegroundColor Yellow
+        New-AzureADMSAdministrativeUnit -DisplayName $namePromotion `
+                                        -Description $namePromotion | Out-Null
+        $idAU = Get-AzureADMSAdministrativeUnit -Filter "DisplayName eq '$namePromotion'" 
+        Write-Host "[+] AU $namePromotion created with success" -ForegroundColor Green
+    }catch{
+        Write-Error $Error[0]
+    }
 
+    try{
+        ExchConnect($Global:AADCredential)
+        Write-Host "[*] Creation of a Microsoft365 group" -ForegroundColor Yellow
+        New-UnifiedGroup -DisplayName $namePromotion `
+                         -Alias $namePromotion `
+                         -AccessType Private `
+                         -PrimarySmtpAddress "$namePromotion@biodevops.tech" `
+                         -Language fr-FR `
+                         -Owner "administrateur@biodevops.tech" | Out-Null
+        Start-Sleep 5
+        $idGRP = Get-UnifiedGroup -Identity $namePromotion
+        AADConnect($Global:AADCredential)
+        Write-Host "[+] The $namePromotion group successfully created" -ForegroundColor Green
+    }catch{
+        Write-Error $Error[0]
+    }
+
+    try{
+        Add-AzureADMSAdministrativeUnitMember -Id $idAU.id `
+                                              -RefObjectId $idGRP.ExternalDirectoryObjectId | Out-Null
+        Write-Host "[+] Adding the $namePromotion group in the $namePromotion AU success " -ForegroundColor Green
+    }catch{
+        Write-Error $Error[0]
+    }
+
+    try{
+        TeamsConnect($Global:AADCredential)
+        New-Team -Group $idGRP.ExternalDirectoryObjectId | Out-Null
+        AADConnect($Global:AADCredential)
+        Write-Host "[+] The Teams $namePromotion group associated with the Microsoft365 group has been activated" -ForegroundColor Green
+    }catch{
+        Write-Error $Error[0]
+    }
+    Write-Host "[+] The $acronymPromotion of $yearPromotion has been created" -ForegroundColor Green
+
+    [int]$userCreation = Read-Host "[1] - Create a unique user`n[2] - Create users in bulk`n[0] - Quit`nEnter an action"
+    Switch($userCreation){
+        1{createUser}
+        2{createUsers}
+        0{break}
+    }
+}
+
+function createUser{
+
+}
+
+function createUsers{
+
+}
 
 # ======== MAIN ========
 # Lancement des pré-requis
